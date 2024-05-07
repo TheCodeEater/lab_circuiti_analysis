08676f42f62730fb19fea2cc6b9f7b1fd0ea2c13
TheCodeEater <35703721+TheCodeEater@users.noreply.github.com>
2024-05-07 22:16:45 +0200

    Aggiunte funzinoi differenza di fase

--- fit_functions.py
+++ fit_functions.py
@@ -21,8 +21,8 @@ woofer_exp = R*V/sy.sqrt(R**2+(w*L)**2)
 tweeter_exp = R*V/sy.sqrt(R**2+(w*C)**-2)
 
 #Phase functions
-woofer_phase_exp = sy.atan(-w*L/R)
-tweeter_phase_exp = sy.atan(1/(w*R*C))
+woofer_phase_exp = sy.atan(-w*L/R)*180/sy.pi
+tweeter_phase_exp = sy.atan(1/(w*R*C))*180/sy.pi
 
 
 #Conversion
@@ -36,6 +36,11 @@ woofer_exp=woofer_exp.subs(R,c.Res2).subs(V,c.Vin).subs(w,f_conv)
 tweeter_phase_exp = tweeter_phase_exp.subs(R,c.Res1).subs(w,f_conv)
 woofer_phase_exp = woofer_phase_exp.subs(R,c.Res2).subs(w,f_conv)
 
+#Commodity functions
+phase_sum_exp=tweeter_phase_exp+woofer_phase_exp
+#phase_diff_exp=sy.abs(tweeter_phase_exp-woofer_phase_exp)
+p_diff=tweeter_phase_exp-woofer_phase_exp
+
 
 #Create lambdas
 woofer_volt = sy.lambdify([f, L], woofer_exp)
@@ -43,5 +48,6 @@ tweeter_volt = sy.lambdify([f, C], tweeter_exp)
 
 tweeter_phase = sy.lambdify([f, C], tweeter_phase_exp)
 woofer_phase = sy.lambdify([f, L], woofer_phase_exp)
+#phase_difference=sy.lambdify([f, L, C], p_diff)
 
 #print(woofer_exp)
